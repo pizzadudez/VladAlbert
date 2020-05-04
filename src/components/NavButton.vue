d5d94536f4ff1a1a8d777aa9698cf810920cf299
unknown <vleadalbert@gmail.com>
2020-05-04 19:48:45 +0300

    hero nav button

---
+++ src/components/NavButton.vue
@@ -0,0 +1,36 @@
+<script>
+export default {};
+</script>
+
+<template>
+  <button>
+    <span>
+      <slot></slot>
+    </span>
+  </button>
+</template>
+
+<style scoped lang="scss">
+button {
+  background: transparent;
+  border-color: rgba(darken($text-color, 30%), 0.4);
+  border-style: solid;
+  border-radius: 4px;
+  cursor: pointer;
+  transition: all 0.3s ease-in-out;
+  span {
+    font-family: $font-primary;
+    font-size: 1.65rem;
+    display: block;
+    color: $text-color;
+    padding: 12px 16px;
+  }
+  &:hover,
+  &:active,
+  &:focus {
+    background: rgba($text-color, 0.07);
+    border-color: darken($text-color, 30%);
+    outline: none;
+  }
+}
+</style>
--- src/components/sections/Hero.vue
+++ src/components/sections/Hero.vue
@@ -1,30 +1,60 @@
 <template>
   <section id="hero">
-    <h1>Vlad Albert</h1>
-    <h2>webdev extraordinaire</h2>
-    <span>is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic</span>
+    <h1>
+      Hello, I'm
+      <span>Vlad Albert</span>
+      .
+    </h1>
+    <h2>I'm a full stack web developer.</h2>
+    <div class="description">
+      <p>I'm a full stack web developerI'm a full stack web developerI'm a full stack web developerI'm a full stack web developerI'm a full stack web developerI'm a full stack web developerI'm a full stack web developer</p>
+    </div>
+    <div class="nav">
+      <nav-button v-scroll-to="'#about'">About</nav-button>
+      <nav-button v-scroll-to="'#projects'">Projects</nav-button>
+      <nav-button>Contact</nav-button>
+      <nav-button>Resume</nav-button>
+    </div>
   </section>
 </template>
 
 <script>
-export default {};
+import NavButton from "../NavButton";
+export default {
+  components: { NavButton }
+};
 </script>
 
 <style scoped lang="scss">
 section {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
   min-height: 100vh;
-  text-align: center;
   h1 {
-    font-size: 3.5rem;
-    color: $text-color;
+    font-size: 4rem;
+    color: $text-white;
+    span {
+      font-size: 5.5rem;
+      color: $text-color;
+    }
   }
   h2 {
-    font-size: 2rem;
+    font-size: 4rem;
     color: $text-white;
   }
-  span {
+  .description {
+    margin-top: 1.8rem;
+    max-width: 600px;
     font-size: 1.5rem;
     color: $text-lightGray;
+  }
+  .nav {
+    margin-top: 2rem;
+    display: flex;
+    button {
+      margin-right: 18px;
+    }
   }
 }
 </style>
